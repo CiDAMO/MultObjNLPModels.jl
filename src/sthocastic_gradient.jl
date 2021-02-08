35f43daa9cdbd924617380e47fa74cd3a097d4a0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-02-08 12:32:04 -0300

    Implement abstract regression models and batch grad!

--- src/sthocastic_gradient.jl
+++ src/sthocastic_gradient.jl
@@ -1,8 +1,5 @@
 using LinearAlgebra, Random, SolverTools
 
-NLPModels.has_bounds(meta::MultObjNLPMeta) = length(meta.ifree) < meta.nvar
-NLPModels.unconstrained(meta::MultObjNLPMeta) = meta.ncon == 0 && !has_bounds(meta)
-
 export sthocastic_gradient
 
 function sthocastic_gradient(
@@ -14,7 +11,7 @@ function sthocastic_gradient(
     penalty::Symbol = :l2,
     max_eval::Int = 0,
     max_time::Float64 = 60.0,
-    max_iter::Int = 100, 
+    max_iter::Int = 100,
     atol::Float64 = 1e-8,
     rtol::Float64 = 1e-8,
     batch_size::Int = 1,
@@ -30,7 +27,6 @@ function sthocastic_gradient(
   n = nlp.meta.nobj
   p = nlp.meta.nvar
   βavg = similar(β)
-  g_tmp = similar(β)
   g = similar(β)
 
   f = NLPModels.obj(nlp, β)
@@ -45,7 +41,7 @@ function sthocastic_gradient(
   elseif penalty == :elasticnet
     β -> ρ * β + (1 - ρ) * sign.(β)
   end
-  
+
   if learning_rate == :optimal && α == 0.0
     @warn("Can't use learning_rate optimal with α = 0.0. Changing to learning_rate step_based.")
     learning_rate = :step_based
@@ -62,16 +58,17 @@ function sthocastic_gradient(
   elseif  learning_rate == :exponential
     iter -> γ0 / exp(d * iter)
   end
-  
+
   status = :unknown
   tired = Δt > max_time || sum_counters(nlp) > max_eval > 0 || iter > max_iter
   small_step = γ(iter) < 1e-6
-  
+
   βavg .= 0
   num_batches = ceil(Int, n / batch_size)
-  
+
   while !(small_step || tired)
 
+    gnorm = -1.0
     Rindex = shuffle(1:n)
     for l = 1:num_batches
       index = if l < num_batches
@@ -79,17 +76,12 @@ function sthocastic_gradient(
       else
         batch_size * (l - 1) + 1:n
       end
-      
-      g .= 0
-      for i in index
-        grad!(nlp, Rindex[i], β, g_tmp)
-        g += g_tmp
-      end
+
+      grad!(nlp, Rindex[index], β, g)
       β -= γ(iter) * (α * P(β) + g / length(index))
       βavg += β
     end
 
-    
     βavg = βavg / num_batches
     Δt = time() - start_time
     iter += 1
@@ -102,7 +94,7 @@ function sthocastic_gradient(
   elseif tired
     if Δt > max_time
       :max_time
-    elseif sum_counters(nlp) > max_eval
+    elseif sum_counters(nlp) > max_eval > 0
       :max_eval
     elseif iter > max_iter
       :max_iter
